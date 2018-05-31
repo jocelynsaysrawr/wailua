cbfd89e976dea13797a34c326c52f8a098df13a2
Jocelyn Belden <jocebelden@gmail.com>
2018-05-30 21:48:08 -1000

    story list component stub

---
+++ src/components/story_list.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class StoryList extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Story List</h2>
+      </div>
+    );
+  }
+}
