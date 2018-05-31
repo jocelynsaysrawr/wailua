12667a5bd0269735aa070a334f0758258c1d1f39
Jocelyn Belden <jocebelden@gmail.com>
2018-05-30 21:24:15 -1000

    header component stub

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,1 +1,11 @@
+import React, { Component } from "react";
 
+export default class AppHeader extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Header</h1>
+      </div>
+    );
+  }
+}
