214a1860e9c7e79c9a60c743a7a6549c54a3e307
Jocelyn Belden <jocebelden@gmail.com>
2018-05-30 21:40:18 -1000

    hamburger navigation component stub

---
+++ src/components/navigation.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class AppNavigation extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hamburger Navigation</h1>
+      </div>
+    );
+  }
+}
