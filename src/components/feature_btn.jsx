e0a6612c9e8a0e1326336ee98e3d6167fdb7114c
Jocelyn Belden <jocebelden@gmail.com>
2018-05-30 21:45:10 -1000

    feature button component stub

---
+++ src/components/feature_btn.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class FeatureBtn extends Component {
+  render() {
+    return (
+      <div>
+        <button>Feature!</button>
+      </div>
+    );
+  }
+}
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import FeatureBtn from "./feature_btn";
 
 export default class AppFooter extends Component {
   render() {
     return (
       <div>
         <h1>Footer</h1>
+        <FeatureBtn />
+        <FeatureBtn />
+        <FeatureBtn />
       </div>
     );
   }
