8acb8144c424321ec949bcdce4b1bbdc1aa1d076
Mark W <markwuu@gmail.com>
2018-06-02 13:40:54 -1000

    refactored map into separate component

---
+++ src/components/map.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+
+export default class Map extends Component {
+
+  render() {
+    const Map = ReactMapboxGl({
+      accessToken: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN,
+      logoPosition: "bottom-left"
+    });
+    return (
+      <div className="map">
+        <Map
+          style="mapbox://styles/mapbox/outdoors-v9"
+          containerStyle={{
+            height: "50vh",
+            width: "100vw"
+          }}
+          center={[-159.5261, 22.0522]}
+          zoom={[9.35]}
+        >
+          <Layer
+            type="symbol"
+            id="marker"
+            layout={{ "icon-image": "marker-15" }}>
+            <Feature coordinates={[-159.34, 22.0522]} />
+            <Feature coordinates={[-159.34, 22.03]} />
+            <Feature coordinates={[-159.38, 22.03]} />
+            <Feature coordinates={[-159.40, 22.05]} />
+            <Feature coordinates={[-159.37, 22.05]} />
+          </Layer>
+        </Map>
+      </div>
+    )
+  }
+}
