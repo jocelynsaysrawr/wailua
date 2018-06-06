3026a1468d068be39dec73b75a609e37c250a67c
Mark W <markwuu@gmail.com>
2018-06-05 19:43:39 -1000

    added slide in react hamburger menu

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AppHeader from "./components/header";
 import AppFooter from "./components/footer";
 import NavList from "./components/nav_list";
 import "./App.css";
+import AppNavigation from "./components/navigation";
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <header className="App-header">
           <AppHeader />
         </header>
+        <AppNavigation />
         <NavList />
         <div className="App-footer">
           <AppFooter />
