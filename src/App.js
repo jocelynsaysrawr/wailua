91adbeb17e3d87f7386d6e99388b21ebd5c7e73c
Mark W <markwuu@gmail.com>
2018-06-06 20:16:27 -1000

    update

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import AppHeader from "./components/header";
 import AppFooter from "./components/footer";
 import "./App.css";
 import Map from "./components/map";
+import HamburgerMenu from "./components/hamburger_menu";
 
 class App extends Component {
 
   render() {
+
+
     return (
       <div className="App">
+        <HamburgerMenu />
         <header className="App-header">
           <AppHeader />
         </header>
-
         <Map />
+
 
         <div className="App-footer">
           <AppFooter />
