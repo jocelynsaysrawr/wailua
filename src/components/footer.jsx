40a5e28700cf2abef3da28b79d7706d4cc657f70
Mark W <markwuu@gmail.com>
2018-06-07 20:17:42 -1000

    gave feature button names

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 import FeatureBtn from "./feature_btn";
 
 export default class AppFooter extends Component {
+
   render() {
     return (
       <div>
         <h1>Footer</h1>
-        <FeatureBtn />
-        <FeatureBtn />
-        <FeatureBtn />
+        <FeatureBtn name={"Story"} />
+        <FeatureBtn name={"Pictures"} />
+        <FeatureBtn name={"Game"} />
       </div>
     );
   }
