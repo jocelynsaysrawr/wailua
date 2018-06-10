1069006632e4e4e4ba30f779b1502b49acd93c33
Jocelyn Belden <jocebelden@gmail.com>
2018-06-09 15:14:41 -1000

    photos branch first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import AppHeader from "./components/header";
 import AppFooter from "./components/footer";
-import NavList from "./components/nav_list";
+//import NavList from "./components/nav_list";
 import "./App.css";
 import Map from "./components/map";
 import HamburgerMenu from "./components/hamburger_menu";
