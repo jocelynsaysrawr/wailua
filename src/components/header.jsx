5ffdd63b1e86ba8a6aa809019eec919cc06c01ac
Mark W <markwuu@gmail.com>
2018-06-19 20:27:24 -1000

    update

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import ProgressBar from "./progress_bar";
+import "../style/header.scss";
 export default class AppHeader extends Component {
   render() {
     return (
-      <div>
+      <div className="header">
         <h1>Wailua</h1>
         <div className="progressCircle">
           <ProgressBar />
