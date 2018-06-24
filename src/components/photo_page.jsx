7cd9f27359d005a855258b34dc28e6e5e0a1df83
Mark W <markwuu@gmail.com>
2018-06-23 18:33:55 -1000

    loading page added

--- src/components/photo_page.jsx
+++ src/components/photo_page.jsx
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
 import { photoAction } from '../actions/index';
 import { connect } from 'react-redux';
+import LoadingScreen from 'react-loading-screen';
 
 class Photos extends Component {
+  state = {
+    loading: true,
+    arrayQuote: [
+      "sharkpoopadoop",
+      "did u finda da ainaa??",
+      "you win da million dollahs!!!!",
+      "mahalo means trash",
+      "aloha for errrrrone",
+      "graduation is nigh",
+      "science you",
+      "add it to the blockchain!!!!!"
+    ],
+    currentQuote: null
+  }
 
   showPics() {
     const photos = this.props.photos;
@@ -12,15 +27,39 @@ class Photos extends Component {
     return showPhoto;
   }
 
-  componentWillMount() {
+  randomNumber() {
+    const number = Math.floor(Math.random() * 8);
+    return number;
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ loading: false })
+    }, 1500);
+
+    this.setState({
+      currentQuote: this.state.arrayQuote[Math.floor(Math.random() * 8)]
+    })
+
     this.props.photoAction();
   }
 
   render() {
+    console.log(this.state.currentQuote);
+    const { loading } = this.state;
+    const number = Math.floor(Math.random() * 8);
     return (
       <div>
-        {this.showPics()}
-      </div>
+        <LoadingScreen
+          loading={loading}
+          bgColor='#373A46'
+          spinnerColor='#9ee5f8'
+          textColor='#ffffff'
+          logoSrc='https://cdn0.iconfinder.com/data/icons/smiley-10/100/Poop-512.png'
+          text={this.state.currentQuote}>
+          {this.showPics()}
+        </LoadingScreen>
+      </div >
     )
   }
 }
